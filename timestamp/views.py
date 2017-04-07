420c95f94f39ba855cfdf700db8099b8ce1dfce3
Lemuel Hawkson <digiwebguy@gmail.com>
2017-04-07 23:06:52 +0000

    changes

--- timestamp/views.py
+++ timestamp/views.py
@@ -4,14 +4,14 @@ from rest_framework.response import Response
 # from rest_framework import status
 # from .models import Timestamp
 # from .serializers import TimestampSerializer
-import datetime
+from django.utils import timezone
 
 
 # timestamp/
 class TimestampList(APIView):
 
     def get(self, request):
-        cur_datetime = str(datetime.datetime.now())
+        cur_datetime = str(timezone.now())
         cur_datetime1 = cur_datetime[2:19]
         cur_datetime2 = cur_datetime[-6:-3]
         cur_datetime = cur_datetime1 + cur_datetime2
