fa8464f4f0ac932c2455b28eaa5fad10f27069f6
Lemuel Hawkson <digiwebguy@gmail.com>
2017-04-08 00:40:32 +0000

    chiange

--- shuttleapi/settings.py
+++ shuttleapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'users.apps.UsersConfig',
     'trips.apps.TripsConfig',
     'topups.apps.TopupsConfig',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -57,6 +59,8 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'shuttleapi.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
