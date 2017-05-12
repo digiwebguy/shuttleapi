2f87a991d16df398b4d5423301b8edf7f434264c
Lemuel Hawkson <digiwebguy@gmail.com>
2017-05-12 15:11:58 +0000

    changed batch payment to one at a time

--- users/views.py
+++ users/views.py
@@ -23,17 +23,32 @@ class UserList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+# https://test-korba-api.herokuapp.com
 class PayForTrips(APIView):
     def post(self, request):
-        print(request.data)
+        print(request.data['card_number'])
+        card_number = request.data['card_number']
 
-        for index_number in request.data:
-            print(index_number + "; ")
-            user = User.objects.get(index_number=index_number)
+        try:
+            user = User.objects.get(index_number=card_number)
+        except User.DoesNotExist:
+            return Response('User Doesnt exist',status=status.HTTP_404_NOT_FOUND)
 
-            print(user)
-            print("old user amount" + str(user.amount))
-            user.amount -= 1
-            print("new user amount" + str(user.amount))
-            user.save()
-        return Response(status=status.HTTP_200_OK)
+        print(user)
+        user.amount -=1
+        user.save()
+        # for index_number in request.data:
+        #     print(index_number + "; ")
+        #     user = User.objects.get(index_number=index_number)
+        #     print(user)
+        #     print("old user amount" + str(user.amount))
+        #     user.amount -= 1
+        #     print("new user amount" + str(user.amount))
+        #     user.save()
+        serializer = UserSerializer(user)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
