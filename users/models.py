e01feecee2c8cd3626acf5271cf3b9ea7bdeae8d
Lemuel Hawkson <digiwebguy@gmail.com>
2017-05-15 21:56:59 +0000

    added uid to index_number

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     firstname = models.CharField(max_length=60)
     lastname = models.CharField(max_length=60)
-    index_number = models.CharField(max_length=8)
+    index_number = models.CharField(max_length=14)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
     amount = models.FloatField(default=0.0)
--- users/views.py
+++ users/views.py
@@ -24,31 +24,30 @@ class UserList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# https://test-korba-api.herokuapp.com
 class PayForTrips(APIView):
     def post(self, request):
         print(request.data)
-        # card_number = request.data['card_number']
+        card_number = request.data['card_number']
 
-        # try:
-        #     user = User.objects.get(index_number=card_number)
-        # except User.DoesNotExist:
-        #     return Response('User Doesnt exist',status=status.HTTP_404_NOT_FOUND)
-        #
-        # print(user)
-        # user.amount -=1
-        # user.save()
-        for index_number in request.data:
-            print(index_number + "; ")
-            user = User.objects.get(index_number=index_number)
-            print(user)
-            print("old user amount" + str(user.amount))
-            user.amount -= 1
-            print("new user amount" + str(user.amount))
-            user.save()
-        serializer = UserSerializer(user)
+        try:
+            user = User.objects.get(index_number=card_number)
+        except User.DoesNotExist:
+            return Response('User Doesnt exist',status=status.HTTP_404_NOT_FOUND)
+
+        print(user)
+        user.amount -=1
+        user.save()
+        # for index_number in request.data:
+        #     print(index_number + "; ")
+        #     user = User.objects.get(index_number=index_number)
+        #     print(user)
+        #     print("old user amount" + str(user.amount))
+        #     user.amount -= 1
+        #     print("new user amount" + str(user.amount))
+        #     user.save()
+        # serializer = UserSerializer(user)
         # if serializer.is_valid():
         #     serializer.save()
         #     return Response(serializer.data, status=status.HTTP_201_CREATED)
         # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
